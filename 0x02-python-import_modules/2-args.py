63d8fb24a6f74b03f88a214b77418964c87f1103
Harold-FN-2187 <laryeaharold319@gmail.com>
2024-01-31 23:17:41 +0000

    minim

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,18 @@
 
 if __name__ == "__main__":
     import sys
+    i = len(sys.argv) - 1
 
-    cnt = len(sys.argv) - 1
-    if cnt == 0:
-        print("{} arguments.".format(cnt))
-    elif cnt == 1:
-        print("{} argument:".format(cnt))
+    if i  == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} arguments:".format(i))
     else:
-        print("{} arguments".format(cnt))
-    for i in range(cnt):
-        print("{}: {}".format(i+1, sys.argv[i + 1]))
+        print("{} arguments:".format(i))
+
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
