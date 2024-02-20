475aa86daf5a7a79e29be7c284b68a26f013466d
Harold-FN-2187 <laryeaharold319@gmail.com>
2024-02-20 21:01:15 +0000

    0x05.0

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0 or not isinstance(a_dictionary, dict):
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
 
-    current_score = list(a_dictionary.keys())[0]
-    best = a_dictionary(current_score)
-
-    for ke_y, val in a_dictionary.items():
-        if ke_y > best:
-            best = val
-            current_score = ke_y
-        return (current_score)
+    ret = list(a_dictionary.keys())[0]
+    big = a_dictionary[ret]
+    for k, v in a_dictionary.items():
+        if v > big:
+            big = v
+            ret = k
+    return (ret)
