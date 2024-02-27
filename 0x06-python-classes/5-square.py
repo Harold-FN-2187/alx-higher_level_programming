66b8f187ea1180a8dbe604a9a8eb7acabfc94e79
Harold-FN-2187 <laryeaharold319@gmail.com>
2024-02-27 14:32:15 +0000

    0x06.4

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
+"""lass that defins a square with some attributes and instantiation"""
 class Square:
     """Represents a Square"""
 
     def __init__(self, size=0):
-
+        """Initializes a new Square instance"""
         self.__size = size
 
     @property
     def size(self):
+        """Retrieve private instance attribute size"""
         return (self.__size)
 
     @size.setter
     def size(self, value):
+        """Retrieves the private instance attribute size"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -19,6 +22,7 @@ class Square:
         self.__size = value
 
     def area(self):
+        """Returns area of Square instance"""
         return (self.__size * self.__size)
 
     def my_print(self):
