20a1ebc7fd76e27bdbe8af1dca1fa2888b3704b8
Harold-FN-2187 <laryeaharold319@gmail.com>
2024-02-27 13:54:21 +0000

    0x06.2

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,1 +1,41 @@
 #!/usr/bin/python3
+class Square:
+    """Represents a Square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @property
+    def position(self):
+        """Retrieve private instance attribute"""
+        return (self.__position)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size mut be >= 0")
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+
+        self.__position = value
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        for j in range(0, self.__size):
+            [print(" ", end="")for k in range(0, self.__position[0])]
+            [print("#", end="") for l in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
