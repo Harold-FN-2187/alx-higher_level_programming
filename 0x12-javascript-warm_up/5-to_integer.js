bc9b30523f585e98730a53a46364d22d5cb01ea1
Harold-FN-2187 <laryeaharold319@gmail.com>
2024-04-09 23:27:48 +0000

    x7.005

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || isNaN(process.argv2)) {
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   console.log('Not a number');
 } else {
   console.log('My number:', parseInt(process.argv[2]));
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 for (const ln of ['C if fun', 'Python is cool', 'JavaScript is amazing']) {
   console.log(ln);
 }
