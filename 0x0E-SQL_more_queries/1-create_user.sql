24cf20ed0e2aaaf0e2970c0de835e32a5efa2e2e
Harold_FN_2187 <laryeaharold319@gmail.com>
2024-04-30 03:39:06 +0000

    x10.002

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,10 @@
--- creates MySQL server user 
+/* -- Creates the user user_0d_1 with all privileges.
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_passwd';
+    IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
    ON *.*
-   TO 'user_0d_1'@'localhost';
+   TO 'user_0d_1'@'localhost' */
+
+
+CREATE DATABASE IF NOT EXISTS `htbn_0d_2`;CREATE USER IF NOT EXISTS 'user_0d_2' @'localhost' IDENTIFIED BY 'password';GRANT ALL PRIVILEGES ON `htbn_0d_2`.* TO 'user_0d_2' @'localhost';
--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,12 +1,15 @@
--- creates database for user user_0d_2
--- grants user only SELECT privileges
+/* -- Creates the database hbtn_0d_2 and the user user_0d_2
+-- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
 CREATE DATABASE
     IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER
     IF NOT EXISTS 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
-    ON `hbtn_0d.2`.*
-    TO 'user_0d_2'@'locahost'
-    IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES;
+   ON `hbtn_0d_2`.*
+   TO 'user_0d_2'@'localhost'
+   IDENTIFIED BY 'user_0d_2_pwd';
+FLUSH PRIVILEGES; */
+
+
+CREATE DATABASE IF NOT EXISTS `htbn_0d_2`;CREATE USER IF NOT EXISTS 'user_0d_2' @'localhost' IDENTIFIED BY 'password';GRANT ALL PRIVILEGES ON `htbn_0d_2`.* TO 'user_0d_2' @'localhost';
