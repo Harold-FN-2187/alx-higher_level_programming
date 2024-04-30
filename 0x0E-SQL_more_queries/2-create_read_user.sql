b1aa30fd4d47f37fe679b320c7f75666f0e34dab
Harold_FN_2187 <laryeaharold319@gmail.com>
2024-04-30 03:41:17 +0000

    x10.003

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,8 @@
-/* -- Creates the database hbtn_0d_2 and the user user_0d_2
+-- Creates the database hbtn_0d_2 and the user user_0d_2
 -- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
-CREATE DATABASE
+
+
+/* CREATE DATABASE
     IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER
     IF NOT EXISTS 'user_0d_2'@'localhost'
@@ -9,7 +11,7 @@ GRANT SELECT
    ON `hbtn_0d_2`.*
    TO 'user_0d_2'@'localhost'
    IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES; */
+FLUSH PRIVILEGES; */ */
 
 
 CREATE DATABASE IF NOT EXISTS `htbn_0d_2`;CREATE USER IF NOT EXISTS 'user_0d_2' @'localhost' IDENTIFIED BY 'password';GRANT ALL PRIVILEGES ON `htbn_0d_2`.* TO 'user_0d_2' @'localhost';
