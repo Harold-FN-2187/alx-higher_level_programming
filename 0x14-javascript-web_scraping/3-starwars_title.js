8c196ae94cf63727f8963353e3f25d8a0de4610d
Harold_FN_2187 <laryeaharold319@gmail.com>
2024-05-22 12:54:16 +0000

    x12.002

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const request = require("request");
-let url = "https://swapi-api.alx-tools.com/api/films/:id" + process.argv[2];
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/:id' + process.argv[2];
 request(url, function (error, response, body) {
   console.log(error || JSON.parse(body).title);
 });
